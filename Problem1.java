cf90169dc39a0bd967dc7bb9807785e11f637df2
Suman2719 <sgupta26195@gmail.com>
2026-05-05 16:25:28 +0530

    Implement maxCyclicSubstringSum method in Problem1

---
+++ Problem1.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Problem1 {
+
+    public static int maxCyclicSubstringSum(String s) {
+        int n = s.length();
+        String doubled = s + s;
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxSum = 0;
+        int currentSum = 0;
+
+        for (int right = 0; right < doubled.length(); right++) {
+            char ch = doubled.charAt(right);
+
+            while (set.contains(ch)) {
+                char leftChar = doubled.charAt(left);
+                currentSum -= (leftChar - 'a' + 1);
+                set.remove(leftChar);
+                left++;
+            }
+
+            set.add(ch);
+            currentSum += (ch - 'a' + 1);
+
+            // Ensure substring length does not exceed original length
+            while (right - left + 1 > n) {
+                char leftChar = doubled.charAt(left);
+                currentSum -= (leftChar - 'a' + 1);
+                set.remove(leftChar);
+                left++;
+            }
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(maxCyclicSubstringSum(s));
+    }
+}
